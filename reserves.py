78bc5463c4bd9e1ce63e4f278679f0c6d286bc57
ibiene-ds <ib.obuoforibo@hotmail.ca>
2023-06-15 04:07:13 -0600

    read and write csv

--- reserves.py
+++ reserves.py
@@ -1,9 +1,28 @@
+import sys 
+import csv
 from math import log10, sqrt
 from enum import Enum
+from typing import List, NamedTuple, Optional, Iterator
+
+OUTPUT_HDR =['Asset_Name', 'Fluid_Phase', 'Original_In_Place', 'EUR']
 
 class Phase(Enum):
     OIL = 0
     GAS = 1
+
+class Asset(NamedTuple):
+    name: str
+    area: float
+    net_height: float
+    phase: Phase
+    recovery_factor: float
+    porosity: float
+    sw: float
+    temperature: float
+    pressure: float
+    oil_api: Optional[float]
+    gas_sg: float 
+
 
 def pseudocritical_temperature(sg: float) -> float:
     # result is in R
@@ -20,7 +39,7 @@ def compressibility(sg: float, p: float, t: float) -> float:
     p_pr = p/ pseudocrititcal_pressure(sg)
     t_pr = (t + 459.67)/pseudocritical_temperature(sg)
     return (1.0 -(3.53 * p_pr)/ (10.0 ** (0.9813 * t_pr))
-             + (0.2774 *p_pr * p_pr)/(10**(0.8157*t_pr)))
+             + (0.2774 * p_pr * p_pr)/(10**(0.8157 * t_pr)))
 
 def gas_fvf(z: float, p: float, t: float)-> float:
     # p is in psi
@@ -54,7 +73,50 @@ def original_in_place(area: float, height: float, porosity: float,
 def eur(original_in_place: float, rf: float)-> float:
     return original_in_place* rf
 
+def read_assets(csv_path: str) -> Iterator[Asset]:
+    with open(csv_path, 'r', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for (name, area, h, phase, rf, phi, sw, temp, pres, api, sg) in reader:
+            yield Asset(name, float(area), float(h), 
+            Phase.OIL if phase == 'oil' else Phase.GAS, 
+            float(rf), float(phi), float(sw), float(temp), float(pres), float(api) if api != '' else None, float(sg))
 
+def asset_original_in_place(asset: Asset) -> float:
+    if asset.phase == Phase.OIL:
+        assert asset.oil_api is not None
+        gor = solution_gor(asset.oil_api, asset.gas_sg, asset.pressure, asset.temperature)
+        fvf = oil_fvf(gor, asset.oil_api, asset.gas_sg, asset.temperature)
+    else:
+        z = compressibility(asset.gas_sg, asset.pressure, asset.temperature)
+        fvf = gas_fvf(z, asset.pressure, asset.temperature)
+    return original_in_place(asset.area, asset.net_height, asset.porosity, asset.sw, fvf, asset.phase)
+    
+
+def main(argv: List[str]) -> int:
+    try:
+        _, src_csv, dst_csv = argv
+    except ValueError:
+        print(f'usage: {argv[0]} src-csv-path dst-csv-path', file = sys.stderr)
+        return 1
+
+    with open(dst_csv, 'w', newline='') as f:
+        w = csv.writer(f)
+        w.writerow(OUTPUT_HDR)
+        for asset in read_assets(src_csv):
+            oip = asset_original_in_place(asset)
+            asset_eur = eur(oip, asset.recovery_factor)
+            w.writerow((
+                asset.name,
+                'oil' if asset.phase == Phase.OIL else 'gas', 
+                oip,
+                asset_eur
+            ))
+
+    return 0 
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
 
 
 
